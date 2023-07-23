f4b31ed4828490eccbba6a7d9de9c3b546a81a26
pourzarrabi <pourzarrabi@gmail.com>
2023-07-23 19:10:25 +0330

    Display Popular Shows

--- js/script.js
+++ js/script.js
@@ -35,14 +35,54 @@ async function displayPopularMovies() {
   });
 }
 
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
+  results.forEach(show => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `<a href="tv-details.html?id=${show.id}">
+      ${
+        show.poster_path
+          ? `<img
+          src="https://image.tmdb.org/t/p/w500/${show.poster_path}"
+          class="card-img-top"
+          alt="${show.name}"
+        />`
+          : `<img
+        src="/images/no-image.jpg"
+        class="card-img-top"
+        alt="${show.name}"
+      />`
+      }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${show.name}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${show.first_air_date}</small>
+      </p>
+    </div>`;
+
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
+
 async function fetchAPIData(endpoint) {
+  showSpinner();
   const response = await fetch(
     `${global.apiUrl}${endpoint}?api_key=${global.apiKey}&language=en-US`
   );
 
   const data = await response.json();
-
+  hideSpinner();
   return data;
+}
+
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
 }
 
 function highlightActiveLink() {
@@ -61,7 +101,7 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
